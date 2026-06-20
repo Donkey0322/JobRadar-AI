c7b423098c0e05b41cb996b8ff212ec435cad9eb
李昀宸 <b09705015@ntu.edu.tw>
2026-06-20 21:10:45 +0800

    fix: update company names and format URLs in job listings

--- src/modules/company-tacker/ats/oraclecloud.ts
+++ src/modules/company-tacker/ats/oraclecloud.ts
@@ -18,6 +18,8 @@ interface OracleCloudJob {
 const identifierMap: Record<string, string> = {
   "fa-ewgu-saasfaprod1.fa.ocs.oraclecloud.com": "Chubb",
   "ekaw.fa.us2.oraclecloud.com": "Securitas",
+  "eofd.fa.us6.oraclecloud.com": "Albertsons Companies",
+  "fa-evmr-saasfaprod1.fa.ocs.oraclecloud.com": "Nokia",
 };
 
 export async function getSiteSettings(url: URL) {
